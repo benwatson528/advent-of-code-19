a2103dd5497140bf29cf50de29c7f82ace444ac2
Ben Watson <benwatson528@gmail.com>
2019-12-01 11:37:28 +0000

    Day 1 part 1

--- src/main/scala/uk/co/hadoopathome/adventofcode19/setup/DaySetup.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/setup/DaySetup.scala
@@ -4,19 +4,16 @@ import scala.io.Source
 import scala.sys.process.Process
 import scala.util.Using
 
-object DaySetup {
+object DaySetup extends App {
+  val day = "1"
+  val puzzleUrl = "https://adventofcode.com/2019/day/" + day
+  val puzzleName = getPuzzleName(puzzleUrl)
 
-  def main(args: Array[String]): Unit = {
-    val day = "1"
-    val puzzleUrl = "https://adventofcode.com/2019/day/" + day
-    val puzzleName = getPuzzleName(puzzleUrl)
-
-    val scriptPath = "src/main/resources/script/create-day.sh"
-    val scriptRunCommand = Set("bash", scriptPath, day, puzzleName).mkString(" ")
-    println("Running day creation command with:" + scriptRunCommand)
-    val process = Process(scriptRunCommand).run()
-    println("Exit value = " + process.exitValue())
-  }
+  val scriptPath = "src/main/resources/script/create-day.sh"
+  val scriptRunCommand = Set("bash", scriptPath, day, puzzleName).mkString(" ")
+  println("Running day creation command: " + scriptRunCommand)
+  val process = Process(scriptRunCommand).run()
+  println("Exit value = " + process.exitValue())
 
   private def getPuzzleName(puzzleUrl: String): String = {
     val entirePage = Using(Source.fromURL(puzzleUrl)) { source => source.mkString }.get
