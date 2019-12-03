dbb8461fe51df6fbae78729dfa8e5d0f2632637a
Ben Watson <benwatson528@gmail.com>
2019-12-03 20:19:46 +0000

    Day 3 part 2

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day03/CrossedWires.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day03/CrossedWires.scala
@@ -4,23 +4,32 @@ object CrossedWires {
 
   private val START_POSITION = new Coord(0, 0)
 
-  def crossWires(wire1: List[String], wire2: List[String]): Int = {
-    val wire1Coords = travel(wire1).toSet
-    val wire2Coords = travel(wire2).toSet
-    val intersectedCoords = wire1Coords.intersect(wire2Coords)
-    intersectedCoords.filterNot(c => c == START_POSITION).map(getManhattanDistance).min
+  def crossWiresManhattan(wire1: List[String], wire2: List[String]): Int = {
+    val wire1Coords = travel(wire1)
+    val wire2Coords = travel(wire2)
+    findIntersections(wire1Coords, wire2Coords).map(getManhattanDistance).min
   }
 
-  def travel(wire: List[String]): IndexedSeq[Coord] =
+  def crossWiresShortest(wire1: List[String], wire2: List[String]): Int = {
+    val wire1Coords = travel(wire1)
+    val wire2Coords = travel(wire2)
+    val intersections = findIntersections(wire1Coords, wire2Coords)
+    intersections.map(i => wire1Coords.indexOf(i) + wire2Coords.indexOf(i)).min
+  }
+
+  private def findIntersections(wire1Coords: IndexedSeq[Coord], wire2Coords: IndexedSeq[Coord]): IndexedSeq[Coord] =
+    wire1Coords.intersect(wire2Coords).filterNot(c => c == START_POSITION)
+
+  private def travel(wire: List[String]): IndexedSeq[Coord] =
     travelRec(wire.map(c => Command(c.head, c.tail.toInt)), IndexedSeq[Coord](START_POSITION))
 
   @scala.annotation.tailrec
-  def travelRec(xs: List[Command], travelledCoords: IndexedSeq[Coord]): IndexedSeq[Coord] = xs match {
+  private def travelRec(xs: List[Command], travelledCoords: IndexedSeq[Coord]): IndexedSeq[Coord] = xs match {
     case x :: xs => travelRec(xs, travelledCoords ++ drawLine(travelledCoords.last, x))
     case _ => travelledCoords
   }
 
-  def drawLine(startCoord: Coord, command: Command): IndexedSeq[Coord] = {
+  private def drawLine(startCoord: Coord, command: Command): IndexedSeq[Coord] = {
     command.direction match {
       case x if x == 'U' =>
         for (i <- startCoord._2 + 1 to startCoord._2 + command.magnitude) yield new Coord(startCoord._1, i)
@@ -33,5 +42,5 @@ object CrossedWires {
     }
   }
 
-  def getManhattanDistance(coord: Coord): Int = coord._1.abs + coord._2.abs
+  private def getManhattanDistance(coord: Coord): Int = coord._1.abs + coord._2.abs
 }
--- src/test/scala/uk/co/hadoopathome/adventofcode19/day03/CrossedWiresTest.scala
+++ src/test/scala/uk/co/hadoopathome/adventofcode19/day03/CrossedWiresTest.scala
@@ -8,24 +8,47 @@ class CrossedWiresTest extends FunSuite {
   test("crossWires simple") {
     val wire1 = stringToList("R8,U5,L5,D3")
     val wire2 = stringToList("U7,R6,D4,L4")
-    assert(6 === CrossedWires.crossWires(wire1, wire2))
+    assert(CrossedWires.crossWiresManhattan(wire1, wire2) === 6)
   }
 
-  test("crossWires complex 1") {
+  test("crossWires manhattan complex 1") {
     val wire1 = stringToList("R75,D30,R83,U83,L12,D49,R71,U7,L72")
     val wire2 = stringToList("U62,R66,U55,R34,D71,R55,D58,R83")
-    assert(159 === CrossedWires.crossWires(wire1, wire2))
+    assert(CrossedWires.crossWiresManhattan(wire1, wire2) === 159)
   }
 
-  test("crossWires complex 2") {
+  test("crossWires part 1 complex 2") {
     val wire1 = stringToList("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
     val wire2 = stringToList("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
-    assert(135 === CrossedWires.crossWires(wire1, wire2))
+    assert(CrossedWires.crossWiresManhattan(wire1, wire2) === 135)
   }
 
   test("crossWires part 1 real") {
     val input = Source.fromResource("day03/input.txt").getLines
-    assert(2193 === CrossedWires.crossWires(stringToList(input.next()), stringToList(input.next())))
+    assert(CrossedWires.crossWiresManhattan(stringToList(input.next()), stringToList(input.next())) === 2193)
+  }
+
+  test("crossWires part 2 simple") {
+    val wire1 = stringToList("R8,U5,L5,D3")
+    val wire2 = stringToList("U7,R6,D4,L4")
+    assert(CrossedWires.crossWiresShortest(wire1, wire2) === 30)
+  }
+
+  test("crossWires part 2 complex 1") {
+    val wire1 = stringToList("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+    val wire2 = stringToList("U62,R66,U55,R34,D71,R55,D58,R83")
+    assert(CrossedWires.crossWiresShortest(wire1, wire2) === 610)
+  }
+
+  test("crossWires part 2 complex 2") {
+    val wire1 = stringToList("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+    val wire2 = stringToList("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+    assert(CrossedWires.crossWiresShortest(wire1, wire2) === 410)
+  }
+
+  test("crossWires part 2 real") {
+    val input = Source.fromResource("day03/input.txt").getLines
+    assert(CrossedWires.crossWiresShortest(stringToList(input.next()), stringToList(input.next())) === 63526)
   }
 
   private def stringToList(s: String): List[String] = s.split(",").toList
