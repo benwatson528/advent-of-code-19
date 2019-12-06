d6e7423319c1fa053ecb63d2b542c5c8ea013e2d
Ben Watson <benwatson528@gmail.com>
2019-12-06 21:45:10 +0000

    Adding optimisation memorisation map

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day06/UniversalOrbitMap.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day06/UniversalOrbitMap.scala
@@ -1,19 +1,30 @@
 package uk.co.hadoopathome.adventofcode19.day06
 
+import scala.collection.mutable
+
 object UniversalOrbitMap {
 
   type Orbit = (String, String)
+  val knownDistances: mutable.Map[String, Int] = scala.collection.mutable.Map[String, Int]()
 
   def countOrbits(ls: List[String]): Int = {
     val orbits = ls.map(x => (x.split(')')(0), x.split(')')(1)))
     val planets = orbits.flatMap(o => Set(o._1, o._2)).distinct
-    planets.map(p => calculateDistanceToCentreRec(p, orbits, 0)).sum
+    planets.map(p => calculateDistanceToCentreRec(p, p, orbits, 0)).sum
   }
 
   @scala.annotation.tailrec
-  private def calculateDistanceToCentreRec(planet: String, orbits: List[Orbit], steps: Int): Int = planet match {
-    case x if (x == "COM") => steps
-    case x => calculateDistanceToCentreRec(getChild(x, orbits), orbits, steps + 1)
+  private def calculateDistanceToCentreRec(startPlanet: String, currentPlanet: String, orbits: List[Orbit], steps: Int):
+  Int = currentPlanet match {
+    case x if (x == "COM") =>
+      knownDistances += (startPlanet -> steps)
+      steps
+    case _ if (knownDistances.contains(currentPlanet)) => {
+      val distanceFromCurrent = knownDistances(currentPlanet)
+      knownDistances += (startPlanet -> (steps + distanceFromCurrent))
+      distanceFromCurrent + steps
+    }
+    case x => calculateDistanceToCentreRec(startPlanet, getChild(x, orbits), orbits, steps + 1)
   }
 
   private def getChild(planet: String, orbits: List[Orbit]): String = orbits.find(_._2 == planet).get._1
