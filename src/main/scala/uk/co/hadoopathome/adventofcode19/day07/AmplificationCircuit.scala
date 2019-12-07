ccfa2df046ead68f270dbac14e8f28a09f50470f
Ben Watson <benwatson528@gmail.com>
2019-12-07 09:40:32 +0000

    Simplifying day 7 part 1

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
@@ -3,15 +3,12 @@ package uk.co.hadoopathome.adventofcode19.day07
 import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids
 
 object AmplificationCircuit {
-  def maxSignal(input: List[Int]): Int = {
-    val phaseSettings = List(0, 1, 2, 3, 4).permutations
-    val outputs = for (ps <- phaseSettings) yield {
-      val outputAmpA = SunnyWithAChanceOfAsteroids.runProgram(input.toIndexedSeq, List(ps.head, 0))
-      val outputAmpB = SunnyWithAChanceOfAsteroids.runProgram(input.toIndexedSeq, List(ps(1), outputAmpA))
-      val outputAmpC = SunnyWithAChanceOfAsteroids.runProgram(input.toIndexedSeq, List(ps(2), outputAmpB))
-      val outputAmpD = SunnyWithAChanceOfAsteroids.runProgram(input.toIndexedSeq, List(ps(3), outputAmpC))
-      SunnyWithAChanceOfAsteroids.runProgram(input.toIndexedSeq, List(ps(4), outputAmpD))
-    }
-    outputs.max
+  def maxSignal(input: IndexedSeq[Int]): Int =
+    List(0, 1, 2, 3, 4).permutations.map(runAmplifierRec(input, 0, _)).max
+
+  @scala.annotation.tailrec
+  private def runAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int = phaseSettings match {
+    case x :: xs => runAmplifierRec(input, SunnyWithAChanceOfAsteroids.runProgram(input, List(x, output)), xs)
+    case Nil => output
   }
 }
--- src/test/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuitTest.scala
+++ src/test/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuitTest.scala
@@ -6,23 +6,24 @@ import scala.io.Source
 
 class AmplificationCircuitTest extends FunSuite {
   test("maxSignal 4,3,2,1,0") {
-    val input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".split(",").map(_.toInt).toList
+    val input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".split(",").map(_.toInt).toIndexedSeq
     assert(AmplificationCircuit.maxSignal(input) === 43210)
   }
 
   test("maxSignal 0,1,2,3,4") {
-    val input = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0".split(",").map(_.toInt).toList
+    val input = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0".split(",").map(_.toInt)
+        .toIndexedSeq
     assert(AmplificationCircuit.maxSignal(input) === 54321)
   }
 
   test("maxSignal 1,0,4,3,2") {
     val input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
-        .split(",").map(_.toInt).toList
+        .split(",").map(_.toInt).toIndexedSeq
     assert(AmplificationCircuit.maxSignal(input) === 65210)
   }
 
   test("sumNumbers real") {
-    val input = Source.fromResource("day07/input.txt").getLines().next().split(",").map(_.toInt).toList
+    val input = Source.fromResource("day07/input.txt").getLines().next().split(",").map(_.toInt).toIndexedSeq
     assert(AmplificationCircuit.maxSignal(input) === 225056)
   }
 }
