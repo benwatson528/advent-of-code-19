dee57218b70a3432d460cd1a1a28bb9191b98744
Ben Watson <benwatson528@gmail.com>
2019-12-08 18:00:35 +0000

    Day 7 part 2 refactored

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
@@ -4,37 +4,38 @@ import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids
 import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids.ProgramState
 
 import scala.collection.mutable
+import scala.collection.mutable.Queue
 
 object AmplificationCircuit {
-  def maxSignalSingleRun(ls: IndexedSeq[Int]): Int =
-    List(0, 1, 2, 3, 4).permutations.map(runAmplifierRec(ls, 0, _)).max
+  def maxSignalSingleRun(ls: IndexedSeq[Int]): Int = List(0, 1, 2, 3, 4).permutations.map(runAmplifierRec(ls, 0, _)).max
 
   def maxSignalFeedbackLoop(ls: IndexedSeq[Int]): Int =
     List(5, 6, 7, 8, 9).permutations.toList.map(runFeedbackLoop(ls, _)).max
 
+
+
   private def runFeedbackLoop(ls: IndexedSeq[Int], phaseSettings: List[Int]): Int = {
     val programStates = initialiseProgramStates(ls, phaseSettings)
-    var lastOutput = -1
-    var i = 0
+    var lastOutput = 0
     while (true) {
-      val returnedProgramState = SunnyWithAChanceOfAsteroids.runProgramPause(programStates(i % 5))
+      val nextProgramState = programStates.dequeue
+      val nextProgramStateWithInput = ProgramState(nextProgramState.pointer, nextProgramState.ls,
+        nextProgramState.inputs :+ lastOutput, None, nextProgramState.isFinished)
+      val returnedProgramState = SunnyWithAChanceOfAsteroids.runProgramPause(nextProgramStateWithInput)
       if (returnedProgramState.isFinished) {
         return lastOutput
       } else {
         lastOutput = returnedProgramState.output.get
       }
-      programStates(i % 5) = returnedProgramState
-      val nextProgramState = programStates((i + 1) % 5)
-      programStates((i + 1) % 5) = ProgramState(nextProgramState.pointer, nextProgramState.ls,
-        nextProgramState.inputs :+ returnedProgramState.output.get, None, nextProgramState.isFinished)
-      i = i + 1
+      programStates.enqueue(returnedProgramState)
     }
     -1
   }
 
-  private def initialiseProgramStates(ls: IndexedSeq[Int], phaseSettings: List[Int]): mutable.Buffer[ProgramState] =
-    (ProgramState(0, ls, List(phaseSettings.head, 0), None, isFinished = false) +:
-        phaseSettings.tail.map(ps => ProgramState(0, ls, List(ps), None, isFinished = false))).toBuffer
+  private def initialiseProgramStates(ls: IndexedSeq[Int], phaseSettings: List[Int]): mutable.Queue[ProgramState] = {
+  //  mutable.Queue[ProgramState](ProgramState(0, ls, List(phaseSettings.head, 0), None, isFinished = false))
+    new mutable.Queue[ProgramState].enqueueAll(phaseSettings.map(ps => ProgramState(0, ls, List(ps), None, isFinished = false)))
+  }
 
   @scala.annotation.tailrec
   private def runAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int =
