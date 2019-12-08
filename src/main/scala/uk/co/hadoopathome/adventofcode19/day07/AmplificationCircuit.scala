9d2a29afd5d80052ff7dc55702432bd89433de7c
Ben Watson <benwatson528@gmail.com>
2019-12-08 15:09:47 +0000

    Day 7 part 2 unfinished

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
@@ -3,12 +3,23 @@ package uk.co.hadoopathome.adventofcode19.day07
 import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids
 
 object AmplificationCircuit {
-  def maxSignal(input: IndexedSeq[Int]): Int =
+  def maxSignalSingleRun(input: IndexedSeq[Int]): Int =
     List(0, 1, 2, 3, 4).permutations.map(runAmplifierRec(input, 0, _)).max
 
+  def maxSignalFeedbackLoop(input: IndexedSeq[Int]): Int =
+    runFeedbackAmplifierRec(input, 0, List(9, 8, 7, 6, 5))
+
   @scala.annotation.tailrec
-  private def runAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int = phaseSettings match {
+  private def runAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int =
+    phaseSettings match {
     case x :: xs => runAmplifierRec(input, SunnyWithAChanceOfAsteroids.runProgram(input, List(x, output)), xs)
     case Nil => output
   }
+
+  @scala.annotation.tailrec
+  private def runFeedbackAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int =
+    phaseSettings match {
+      case x :: xs => runFeedbackAmplifierRec(input, SunnyWithAChanceOfAsteroids.runProgram(input, List(x, output)), xs)
+      case Nil => output
+    }
 }
