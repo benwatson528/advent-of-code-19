3814f6767a5c6ab65884cd04f031175f04e60b0b
Ben Watson <benwatson528@gmail.com>
2019-12-08 18:04:20 +0000

    Day 7 part 2 refactored

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day07/AmplificationCircuit.scala
@@ -4,14 +4,12 @@ import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids
 import uk.co.hadoopathome.adventofcode19.day05.SunnyWithAChanceOfAsteroids.ProgramState
 
 import scala.collection.mutable
-import scala.collection.mutable.Queue
 
 object AmplificationCircuit {
   def maxSignalSingleRun(ls: IndexedSeq[Int]): Int = List(0, 1, 2, 3, 4).permutations.map(runAmplifierRec(ls, 0, _)).max
 
   def maxSignalFeedbackLoop(ls: IndexedSeq[Int]): Int =
     List(5, 6, 7, 8, 9).permutations.toList.map(runFeedbackLoop(ls, _)).max
-
 
 
   private def runFeedbackLoop(ls: IndexedSeq[Int], phaseSettings: List[Int]): Int = {
@@ -33,14 +31,13 @@ object AmplificationCircuit {
   }
 
   private def initialiseProgramStates(ls: IndexedSeq[Int], phaseSettings: List[Int]): mutable.Queue[ProgramState] = {
-  //  mutable.Queue[ProgramState](ProgramState(0, ls, List(phaseSettings.head, 0), None, isFinished = false))
     new mutable.Queue[ProgramState].enqueueAll(phaseSettings.map(ps => ProgramState(0, ls, List(ps), None, isFinished = false)))
   }
 
   @scala.annotation.tailrec
-  private def runAmplifierRec(input: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int =
+  private def runAmplifierRec(ls: IndexedSeq[Int], output: Int, phaseSettings: List[Int]): Int =
     phaseSettings match {
-      case x :: xs => runAmplifierRec(input, SunnyWithAChanceOfAsteroids.runProgram(input, List(x, output)), xs)
+      case x :: xs => runAmplifierRec(ls, SunnyWithAChanceOfAsteroids.runProgram(ls, List(x, output)), xs)
       case Nil => output
     }
 }
