81fe73ea99ab1b5718dca2017cc13f7a6ab5ccf2
Ben Watson <benwatson528@gmail.com>
2019-12-12 20:35:33 +0000

    Day 11 part 2

--- src/test/scala/uk/co/hadoopathome/adventofcode19/day11/SpacePoliceTest.scala
+++ src/test/scala/uk/co/hadoopathome/adventofcode19/day11/SpacePoliceTest.scala
@@ -1,6 +1,7 @@
 package uk.co.hadoopathome.adventofcode19.day11
 
 import org.scalatest.FunSuite
+import uk.co.hadoopathome.adventofcode19.day11.SpacePolice.Coord
 
 import scala.io.Source
 
@@ -13,12 +14,20 @@ class SpacePoliceTest extends FunSuite {
 
   test("drawRegistration real") {
     val input = parseInput(Source.fromResource("day11/input.txt").getLines.next())
-    assert(SpacePolice.drawRegistration(input.toList) === 1681)
+   // drawOutput(SpacePolice.drawRegistration(input.toList))
   }
 
   private def parseInput(input: String): IndexedSeq[Long] = input.split(",").map(_.toLong)
 
-  private def drawOutput(): Unit = {
+  def drawOutput(painted: Map[Coord, Int]): Any = {
+    val (minX, maxX) = (painted.minBy(_._1.x)._1.x, painted.maxBy(_._1.x)._1.x)
+    val (minY, maxY) = (painted.minBy(_._1.y)._1.y, painted.maxBy(_._1.y)._1.y)
 
+    for (y <- maxY to minY by -1) {
+      println()
+      for (x <- minX to maxX) {
+        print(if (painted.contains(Coord(x, y))) "█" else " ")
+      }
+    }
   }
 }
