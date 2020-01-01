0cbbf5cd868b46b27c7f436d5f0dab41492963be
Ben Watson <benwatson528@gmail.com>
2020-01-01 18:30:38 +0000

    Commenting broken day

--- src/main/scala/uk/co/hadoopathome/adventofcode19/day18/ManyWorldsInterpretation.scala
+++ src/main/scala/uk/co/hadoopathome/adventofcode19/day18/ManyWorldsInterpretation.scala
@@ -1,41 +1,41 @@
-package uk.co.hadoopathome.adventofcode19.day18
-
-object ManyWorldsInterpretation {
-
-  def collectAllKeys(maze: Maze): Int = {
-    val entrance = maze.find(_._2 == '@').get._1
-    val totalKeys = maze.count(_._2.isLower)
-    moveRec(maze, entrance, List[Coord](), Set[Char](), totalKeys)
-  }
-
-  private def moveRec(maze: Maze, currentPosition: Coord, visited: List[Coord], collectedKeys: Set[Char],
-                      totalKeys: Int, newKeys: Int, keyPickupPoint: Int): Int = {
-    if (collectedKeys.size == totalKeys) visited.length
-    else if (visited.contains())
-    else {
-      currentPosition match {
-        case '#' => 10000
-        case '.' =>
-          getNextMoves(currentPosition)
-              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys, totalKeys))
-              .min
-        case x if maze(x).isLower =>
-          getNextMoves(currentPosition)
-              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys + maze(currentPosition), totalKeys))
-              .min
-        case x if maze(x).isUpper =>
-          if (collectedKeys.contains(maze(x))) getNextMoves(currentPosition)
-              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys, totalKeys))
-              .min
-          else 10000
-      }
-    }
-  }
-
-  private def getNextMoves(currentPosition: Coord) = {
-    Set(currentPosition.copy(x = currentPosition.x + 1),
-      currentPosition.copy(x = currentPosition.x - 1),
-      currentPosition.copy(y = currentPosition.y + 1),
-      currentPosition.copy(y = currentPosition.y - 1))
-  }
-}
+//package uk.co.hadoopathome.adventofcode19.day18
+//
+//object ManyWorldsInterpretation {
+//
+//  def collectAllKeys(maze: Maze): Int = {
+//    val entrance = maze.find(_._2 == '@').get._1
+//    val totalKeys = maze.count(_._2.isLower)
+//    moveRec(maze, entrance, List[Coord](), Set[Char](), totalKeys)
+//  }
+//
+//  private def moveRec(maze: Maze, currentPosition: Coord, visited: List[Coord], collectedKeys: Set[Char],
+//                      totalKeys: Int, newKeys: Int, keyPickupPoint: Int): Int = {
+//    if (collectedKeys.size == totalKeys) visited.length
+//    else if (visited.contains())
+//    else {
+//      currentPosition match {
+//        case '#' => 10000
+//        case '.' =>
+//          getNextMoves(currentPosition)
+//              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys, totalKeys))
+//              .min
+//        case x if maze(x).isLower =>
+//          getNextMoves(currentPosition)
+//              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys + maze(currentPosition), totalKeys))
+//              .min
+//        case x if maze(x).isUpper =>
+//          if (collectedKeys.contains(maze(x))) getNextMoves(currentPosition)
+//              .map(moveRec(maze, _, currentPosition +: visited, collectedKeys, totalKeys))
+//              .min
+//          else 10000
+//      }
+//    }
+//  }
+//
+//  private def getNextMoves(currentPosition: Coord) = {
+//    Set(currentPosition.copy(x = currentPosition.x + 1),
+//      currentPosition.copy(x = currentPosition.x - 1),
+//      currentPosition.copy(y = currentPosition.y + 1),
+//      currentPosition.copy(y = currentPosition.y - 1))
+//  }
+//}
